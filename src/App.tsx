9aaa6437eff104bcaf2d3a74fe82c504163ac7b4
Konstantin <konstantin.muenster@mail.com>
2022-11-24 06:55:55 +0100

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="max-w-4xl mx-auto my-16">
+      <h1 className="text-3xl font-serif">Lexical React Demo</h1>
+    </div>
+  );
+}
+
+export default App;
