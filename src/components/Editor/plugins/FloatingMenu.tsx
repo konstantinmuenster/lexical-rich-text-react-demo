478b3b9930afcf2b773011a1845454da7d103856
Konstantin <konstantin.muenster@mail.com>
2022-12-06 06:17:05 +0100

    add aria hidden to floating menu

--- src/components/Editor/plugins/FloatingMenu.tsx
+++ src/components/Editor/plugins/FloatingMenu.tsx
@@ -50,6 +50,7 @@ function FloatingMenu({ show, ...props }: FloatingMenuProps) {
     <div
       ref={ref}
       style={{ top: pos?.y, left: pos?.x }}
+      aria-hidden={!pos?.x || !pos?.y}
       className={`absolute flex items-center justify-between bg-slate-100 border-[1px] border-slate-300 rounded-md p-1 gap-1 ${
         pos?.x && pos.y ? "opacity-1 visible" : "opacity-0 invisible"
       }`}
